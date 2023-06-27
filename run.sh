9f2e0190c415008162a9618e92d069d4a3926cb7
Matthieu Rochette <matthieu.rochette@epitech.eu>
2023-06-27 21:40:19 +0200

    add mini script to run server so not forgetting the commands

---
+++ run.sh
@@ -0,0 +1,10 @@
+
+echo "URL of deployment is:" $1
+
+if [ -z "$1" ] ; then
+    echo "Please provide the base URL of the deployment as the first parameter";
+    exit 1
+fi
+
+docker build -t pblive .
+docker run --rm -e "QUIZ_SERVER_URL="$1 -p 5000:5000 pblive
